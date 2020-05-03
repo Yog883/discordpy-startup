17f2eb462aab55484af18ecf614d91b64c54b10d
Yog883 <64743322+Yog883@users.noreply.github.com>
2020-05-04 01:36:50 +0900

    backmessages_C&P

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,5 @@
+#Discord.pyの読み込み
+import discord
 from discord.ext import commands
 import os
 import traceback
@@ -15,7 +17,21 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pongpong')
+    await ctx.send('pong2')
 
+#メッセージを取得した時に実行される
+@client.event
+async def on_message(message): 
 
-bot.run(token)
+    #条件に当てはまるメッセージかチェックし正しい場合は返す
+    def check(msg):
+        return msg.author == message.author
+    
+    #ユーザーからのメッセージを待つ
+    wait_message = await client.wait_for("message", check=check)
+
+    #取得したメッセージを書き込まれたチャンネルへ送信
+    await message.channel.send(wait_message.content)
+
+#Botの実行
+client.run(TOKEN)
