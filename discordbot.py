9640d4b6e8c04c29ee35e96660345cbf067be94a
Yog883 <64743322+Yog883@users.noreply.github.com>
2020-05-04 01:41:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,18 +20,18 @@ async def ping(ctx):
     await ctx.send('pong2')
 
 #メッセージを取得した時に実行される
-@client.event
-async def on_message(message): 
-
-    #条件に当てはまるメッセージかチェックし正しい場合は返す
-    def check(msg):
-        return msg.author == message.author
-    
-    #ユーザーからのメッセージを待つ
-    wait_message = await client.wait_for("message", check=check)
-
-    #取得したメッセージを書き込まれたチャンネルへ送信
-    await message.channel.send(wait_message.content)
+#@client.event
+#async def on_message(message): 
+#
+#    #条件に当てはまるメッセージかチェックし正しい場合は返す
+#    def check(msg):
+#        return msg.author == message.author
+#    
+#    #ユーザーからのメッセージを待つ
+#    wait_message = await client.wait_for("message", check=check)
+#
+#    #取得したメッセージを書き込まれたチャンネルへ送信
+#    await message.channel.send(wait_message.content)
 
 #Botの実行
 client.run(TOKEN)
